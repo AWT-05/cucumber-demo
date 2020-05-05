40b40c76b938bb3fdb52d7913d92b50809e9856b
Snip-77 <mau.oroza1@gmail.com>
2020-05-05 17:00:07 -0400

    Fixes

--- src/test/java/org/fundacionjala/cucumber/demo/stepdefs/CreateNewUserSteps.java
+++ src/test/java/org/fundacionjala/cucumber/demo/stepdefs/CreateNewUserSteps.java
@@ -1,5 +1,7 @@
 package org.fundacionjala.cucumber.demo.stepdefs;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -8,6 +10,11 @@ import io.cucumber.java.en.When;
 import static org.testng.Assert.assertEquals;
 
 public class CreateNewUserSteps {
+
+    @Before("@Acceptance")
+    public void setUp() {
+        System.out.println("Before: All steps in a Scenario");
+    }
 
     @Given("I set authentication token using admin account")
     public void authenticate() {
@@ -22,5 +29,10 @@ public class CreateNewUserSteps {
     @Then("II validate the response has status code 200")
     public void validateStatusCode(int statusCode) {
         assertEquals(statusCode,200);
+    }
+
+    @After("@Acceptance")
+    public void endFeature() {
+        System.out.println("Cleaning...");
     }
 }
